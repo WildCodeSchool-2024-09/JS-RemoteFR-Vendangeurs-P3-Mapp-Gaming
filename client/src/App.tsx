38f041a27e09c1a013a5bd87165ebfd1eff0b6a7
AURELIA GUARINO <aureliaguarino0@gmail.com>
2025-01-10 17:08:28 +0100

    [feat] add navbar and footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 
-import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <div>
