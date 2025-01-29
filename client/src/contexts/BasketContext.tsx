fc38359a74ea43de665067be14f3a4b908aeae24
Benoît Vandanjon <benoit@vandanjon.fr>
2025-01-29 09:49:08 +0100

    [FEAT] : add context logic for basket

--- client/src/contexts/BasketContext.tsx
+++ client/src/contexts/BasketContext.tsx
@@ -1,11 +1,39 @@
-function BasketContext() {
-  return (
-    <div id="BasketContextContainer">
-      <h1>BasketContext</h1>
+import { createContext, useContext, useState } from "react";
+import type { ReactNode } from "react";
 
-      <p>BasketContext Page</p>
-    </div>
+type VideoGame = { id: number; title: string; price: number };
+type Basket = { videoGames: VideoGame[]; userId: number };
+
+type BasketContextType = {
+  basket: Basket;
+  addToBasket: (videogame: VideoGame) => void;
+};
+
+const BasketContext = createContext<BasketContextType | undefined>(undefined);
+
+export const BasketProvider = ({ children }: { children: ReactNode }) => {
+  const [basket, setBasket] = useState<Basket>({ videoGames: [], userId: 0 });
+
+  const addToBasket = (videoGame: VideoGame) => {
+    setBasket({
+      videoGames: [...basket.videoGames, videoGame],
+      userId: basket.userId,
+    });
+  };
+
+  return (
+    <BasketContext.Provider value={{ basket, addToBasket }}>
+      {children}
+    </BasketContext.Provider>
   );
-}
+};
+
+export const useBasket = () => {
+  const context = useContext(BasketContext);
+  if (context === undefined) {
+    throw new Error("useBasket must be used within a BasketProvider");
+  }
+  return context;
+};
 
 export default BasketContext;
