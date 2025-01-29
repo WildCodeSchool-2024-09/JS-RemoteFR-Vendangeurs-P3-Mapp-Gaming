734c31811ac322e0fd669d51705008cbd017232d
Benoît Vandanjon <benoit@vandanjon.fr>
2025-01-29 09:22:55 +0100

    [FEAT] : prepare Basket Context base page

---
+++ client/src/contexts/BasketContext.tsx
@@ -0,0 +1,11 @@
+function BasketContext() {
+  return (
+    <div id="BasketContextContainer">
+      <h1>BasketContext</h1>
+
+      <p>BasketContext Page</p>
+    </div>
+  );
+}
+
+export default BasketContext;
